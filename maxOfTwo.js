e633d20fd0ae81b6701327f2a1132c371b973f78
kallan-randall <kallan92@gmail.com>
2023-03-20 15:18:56 -0600

    finished project

---
+++ maxOfTwo.js
@@ -0,0 +1,19 @@
+let x = 1
+let y = 2
+
+// Write your code below this line.
+if(x > y) {
+    console.log(x)
+} else {
+    console.log(y)
+}
+
+//Additional challenge
+
+if (x > y) {
+    console.log(x)
+} else if (y > x) {
+    console.log(y)
+} else {
+    console.log('The values are identical')
+}
